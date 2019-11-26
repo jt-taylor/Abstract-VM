3714046815e95e9bd528e90d48e79b1abbd074d4
Jackson Taylor <jtttaylorjackson@gmail.com>
2019-11-25 19:46:25 -0800

    removed a bad check when reading from the standart input, was always comparing against a null string

--- main.cpp
+++ main.cpp
@@ -14,11 +14,8 @@ static void	read_from_cin_to_vector(std::vector<std::string> & v)
 	std::string tmp;
 	while (std::getline(std::cin, tmp))
 		v.push_back(tmp);
-	auto t = v.end();
 	//if (t->compare(0, 2, ";;") == 0)
 	if (v.empty())
-		throw (ex_NoExitInstruction());
-	if (t->find(std::string(";;"), 0) != std::string::npos)
 		throw (ex_NoExitInstruction());
 }
 
